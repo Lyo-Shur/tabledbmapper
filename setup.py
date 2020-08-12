d2d64c3629735fb9d0c0f2e096c3881dad84ab3f
lyoshur <lyoshur@foxmail.com>
2020-08-12 13:58:44 +0800

    修改版本号

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
     setup(
         name="tabledbmapper",
-        version="0.2.0",
+        version="0.2.1",
         description=(
             "General query tool class of relational database."
         ),
